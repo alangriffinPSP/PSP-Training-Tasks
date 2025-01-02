0cf7c6b0a294aa9cbcf9eee45f35ce2594ee72a6
Alan Griffin <alan.griffin@petsitterplus.com>
2025-01-02 21:00:30 +0000

    Utilised classes

--- Task 6/taskSix.js
+++ Task 6/taskSix.js
@@ -1,10 +1,9 @@
-$(document).ready(function(){
-
 //Object for methods to run on page load
-const $pageLoad = {
+class $PageLoad {
+  constructor(){}
   $clearForm(){
   $('#submission-form').trigger('reset')
-  },
+  }
 
   $themeSet(){
     if($('html').attr('data-theme') == 'dark') {
@@ -14,9 +13,12 @@ const $pageLoad = {
     }
   }
 }
+//New object created from PageLoad class
+const $freshPage = new $PageLoad();
 
-const $userInteraction = {
-//Theme change method
+class $UserInteraction {
+  constructor(){}
+  //Theme change method
   $changeTheme(){
   if($('html').attr('data-theme') == 'dark') {
     ($('html').attr('data-theme', 'light'));
@@ -25,19 +27,19 @@ const $userInteraction = {
     ($('html').attr('data-theme', 'dark'));
     $('#theme-change').html('LIGHTER');
   }
-},
+}
 //Content creation method
   $createNode(){  
-  $newTitle = $('#new-title').val();
-  $newContent = $('#new-article').val();
-  $newDate = new Date().toLocaleDateString('en-gb', { 
+  const $newTitle = $('#new-title').val();
+  const $newContent = $('#new-article').val();
+  const $newDate = new Date().toLocaleDateString('en-gb', { 
     weekday:"long", 
     day:"numeric", 
     month:"long", 
     year:"numeric",
   });
 
-$newNode = 
+const $newNode = 
 $( "<div>", { class: "article-group", html:
 ([
   $('<h6>',{ class: "article-date", text: $newDate }),
@@ -49,18 +51,18 @@ $( "<div>", { class: "article-group", html:
  if ($newTitle && $newContent != ''){
 $('.article-group:first').before($newNode);
   $(window).scrollTop(0);
-  $pageLoad.$clearForm();
+  $freshPage.$clearForm();
  }
 } 
-} 
+}
+
+//New object created from UserInteraction class
+const $interact = new $UserInteraction();
 
 //User controlled method calls
-$('#article-submit').click($userInteraction.$createNode);
-$("#theme-change").click($userInteraction.$changeTheme);
+$('#article-submit').click($interact.$createNode);
+$("#theme-change").click($interact.$changeTheme);
 
 //Methods run on page load
-$pageLoad.$clearForm();
-$pageLoad.$themeSet(); 
-
-//End of document ready
-})
+$freshPage.$clearForm();
+$freshPage.$themeSet();
